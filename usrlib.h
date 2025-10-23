ffb31a21a65bbc4e7fa24726222e3472c9df9995
BusyBox13 <maxime.moreau.info+github@gmail.com>
2025-10-23 22:04:59 +0200

    Add utility to get arguments fo struct pt_regs

--- usrlib.h
+++ usrlib.h
@@ -16,4 +16,17 @@
 #define warn(...)
 #endif
 
+/* Get the 1st arg of a `struct pt_regs` */
+#define ARG1(regs, cast) (cast) regs->di
+/* Get the 2nd arg of a `struct pt_regs` */
+#define ARG2(regs, cast) (cast) regs->si
+/* Get the 3rd arg of a `struct pt_regs` */
+#define ARG3(regs, cast) (cast) regs->dx
+/* Get the 4th arg of a `struct pt_regs` */
+#define ARG4(regs, cast) (cast) regs->r10
+/* Get the 5th arg of a `struct pt_regs` */
+#define ARG5(regs, cast) (cast) regs->r8
+/* Get the 6th arg of a `struct pt_regs` */
+#define ARG6(regs, cast) (cast) regs->r9
+
 #endif /* USRLIB_H */

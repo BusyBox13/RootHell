1ac6bef509ac8f3522585751bc357a33a9bed4d4
BusyBox13 <maxime.moreau.info+github@gmail.com>
2025-10-24 10:47:55 +0200

    Update commands: add new commands

--- commands.h
+++ commands.h
@@ -1,11 +1,23 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-static int signal = 42;
+/**
+ * Signal code used to trigger specific function
+ */
+enum sig
+{
+    SIG_SHOW_MYSELF = 42, /* Show/Hide this module */
+    SIG_HIDE_PROC,
+    SIG_SET_USER
+} sig;
 
+/**
+ * Code to protect command (as password)
+ * Or to do multiple actions
+ */
 enum code
 {
-    SHOW_MYSELF = 666
+    SHOW_MYSELF = 666 /* Show/Hide this module */
 } code;
 
 #define PREFIX "this."
